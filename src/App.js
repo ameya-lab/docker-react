1eb222700e66e476e7c46aeb9da52245ba98a045
ameya-lab <ameyad22@gmail.com>
2020-04-14 14:54:42 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Its my first react project
         </a>
       </header>
     </div>
